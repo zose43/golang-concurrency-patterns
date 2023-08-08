6e4532459dba71da2c3a3edc08082fd482b18221
Kirill Zorin <thesecretsoftheuniverse@mail.ru>
2023-08-08 22:37:29 +0300

    add convert dataset method

--- worker-pool/dataset.go
+++ worker-pool/dataset.go
@@ -4,7 +4,23 @@ import (
 	"encoding/json"
 	"fmt"
 	"golang-concurrency-patterns/lib/storage"
+	"log"
+	"os"
+	"path"
 )
+
+type dataset struct {
+	Name  string `json:"name"`
+	Id    int    `json:"id"`
+	Email string `json:"email"`
+	Phone string `json:"phone_number"`
+}
+
+type client struct {
+	Name  string `json:"name"`
+	Id    int    `json:"id"`
+	Email string `json:"email"`
+}
 
 func openDataset() (users []User, phones map[string]string, err error) {
 	userBt, err := storage.OpenData("clients.json")
@@ -25,4 +41,43 @@ func openDataset() (users []User, phones map[string]string, err error) {
 		return nil, nil, fmt.Errorf("can't unmarshall phones %w", err)
 	}
 	return
+}
+
+func convert() {
+	bytes, err := os.ReadFile(path.Join(storage.Dir, "dataset.json"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var elems []dataset
+	if err = json.Unmarshal(bytes, &elems); err != nil {
+		log.Fatal(err)
+	}
+
+	clients := make([]client, len(elems))
+	phones := make(map[string]string, len(elems))
+	for i, elem := range elems {
+		phones[elem.Name] = elem.Phone
+		clients[i] = client{Name: elem.Name, Id: elem.Id, Email: elem.Email}
+	}
+
+	f, err := os.Create(path.Join(storage.Dir, "clients.json"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	clientsBt, _ := json.Marshal(clients)
+	_, err = f.Write(clientsBt)
+	if err != nil {
+		log.Print("can't save clients")
+	}
+
+	f, err = os.Create(path.Join(storage.Dir, "phone_numbers.json"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	phonesBt, _ := json.Marshal(phones)
+	_, err = f.Write(phonesBt)
+	if err != nil {
+		log.Print("can't save phones")
+	}
 }
