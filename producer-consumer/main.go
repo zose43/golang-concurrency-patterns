7bd65c9bf55b47ebfb8460a8789e3557eb236ddf
Kirill Zorin <thesecretsoftheuniverse@mail.ru>
2023-08-13 13:07:31 +0300

    add csv parser

--- producer-consumer/main.go
+++ producer-consumer/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
+	"golang-concurrency-patterns/lib/storage"
 	"log"
 	"time"
 )
@@ -14,7 +16,7 @@ func main() {
 		log.Fatal("not initialized list")
 	}
 
-	streamCh := make(chan *Stream, 10)
+	//streamCh := make(chan *Stream, 10)
 	done := make(chan struct{})
 
 	<-done
@@ -22,7 +24,35 @@ func main() {
 }
 
 func init() {
-	// todo parse csv and fill stream linked list
+	csvF, err := storage.OpenFile("airport_data.csv")
+	if err != nil {
+		log.Fatal("can't load dataset")
+	}
+	defer func() { _ = csvF.Close() }()
+
+	reader := csv.NewReader(csvF)
+	items, err := reader.ReadAll()
+	if err != nil {
+		log.Fatalf("can't read file %s", err)
+	}
+
+	list = &LinkedList{nil, 0}
+	for _, el := range items {
+		if "#" != el[0] {
+			dt, err := time.Parse("2006-01-02 15:04:05 -07:00", el[4])
+			if err != nil {
+				log.Printf("can't parse date %s", err)
+				continue
+			}
+			stream := &Stream{
+				airport:  el[1],
+				city:     el[2],
+				timezone: el[3],
+				date:     dt,
+			}
+			list.addToHead(stream)
+		}
+	}
 }
 
 func producer() {
